914ec86d43c8a0692e20d7b5efd34ac402def539
seo337dc <sdc337dc@gmail.com>
2025-01-06 17:47:09 +0900

    다시 롤백

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import Introduce from "@/components/views/Introduce";
 import PartnerSection from "@/components/views/Partner";
 import ProjectsSection from "@/components/views/Projects";
 import RWA from "@/components/views/RWA";
-// import MainView from "@/components/views/main";
+import MainView from "@/components/views/main";
 
 import Footer from "@/components/views/Footer";
 import Layout from "@/components/common/Layout";
